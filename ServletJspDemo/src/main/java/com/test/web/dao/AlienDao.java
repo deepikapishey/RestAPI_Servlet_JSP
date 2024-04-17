a50149757487ec0c95fab65734e889159676854d
deepikapishey <147231860+deepikapishey@users.noreply.github.com>
2024-04-18 01:12:27 +0530

    Add files via upload

---
+++ ServletJspDemo/src/main/java/com/test/web/dao/AlienDao.java
@@ -0,0 +1,36 @@
+package com.test.web.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.test.web.model.Alien;
+
+public class AlienDao {
+	
+	public Alien getAlien(int aid)
+	{
+		Alien a=new Alien();
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/deepi","root","your_password");
+			Statement st=con.createStatement();
+			ResultSet rs=st.executeQuery("Select * from alien where aid="+aid);
+			if(rs.next())
+			{
+				a.setAid(rs.getInt("aid"));
+				a.setAname(rs.getString("aname"));
+				a.setTech(rs.getString("tech"));
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return a;
+	}
+
+}
